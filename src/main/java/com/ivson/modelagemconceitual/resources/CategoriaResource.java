4714104ed473fcc10d0e4a983b9a78f1c461e2bc
Ivson Santos <santosivson@hotmail.com>
2018-05-20 11:17:19 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/ivson/modelagemconceitual/resources/CategoriaResource.java
+++ src/main/java/com/ivson/modelagemconceitual/resources/CategoriaResource.java
@@ -5,13 +5,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -78,6 +81,18 @@ public class CategoriaResource {
 		List<CategoriaDTO> listDTO = list.stream().map
 				(obg -> new CategoriaDTO(obg)).collect(Collectors.toList());
 		
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@GetMapping("/page")
+	public ResponseEntity<Page<CategoriaDTO>> findpage(
+			@RequestParam(value="page", defaultValue="0") Integer page, // opcional, se nao informar, vai pra primeira pagina (0)
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {		
+
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);		
+		Page<CategoriaDTO> listDTO = list.map(obg -> new CategoriaDTO(obg));		
 		return ResponseEntity.ok().body(listDTO);
 	}
 	
