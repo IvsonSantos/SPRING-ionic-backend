a54cb0fedea6d846308d99d5bcb4f4a5fd5db83e
Ivson Santos <santosivson@hotmail.com>
2018-05-19 21:54:24 -0300

    Deletado a Categoria com tratamento de excecao

--- src/main/java/com/ivson/modelagemconceitual/services/CategoriaService.java
+++ src/main/java/com/ivson/modelagemconceitual/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.ivson.modelagemconceitual.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.ivson.modelagemconceitual.model.Categoria;
 import com.ivson.modelagemconceitual.repositories.CategoriaRepository;
+import com.ivson.modelagemconceitual.services.exceptions.DataIntegrityException;
 import com.ivson.modelagemconceitual.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -27,9 +29,18 @@ public class CategoriaService {
 		return repo.save(categoria);
 	}
 	
-	public Categoria update(Categoria categoria) {
-		
+	public Categoria update(Categoria categoria) {		
 		find(categoria.getId());		
 		return repo.save(categoria);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		
+		try {		
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possuir produtos");
+		}
 	}
 }
