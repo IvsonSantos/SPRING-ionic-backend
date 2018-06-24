eb71aa692d30046e497d94aeb319fc0ae42e8654
Ivson Santos <santosivson@hotmail.com>
2018-06-24 17:22:48 -0300

    Email com HTML criado

--- src/main/java/com/ivson/modelagemconceitual/services/AbstractEmailService.java
+++ src/main/java/com/ivson/modelagemconceitual/services/AbstractEmailService.java
@@ -2,8 +2,16 @@ package com.ivson.modelagemconceitual.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.ivson.modelagemconceitual.model.Pedido;
 
@@ -11,6 +19,12 @@ public abstract class AbstractEmailService implements EmailService {
 	
 	@Value("${default.sender}")
 	private String sender;
+	
+	@Autowired
+	private TemplateEngine templateEngine; 	
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 	
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
@@ -30,5 +44,37 @@ public abstract class AbstractEmailService implements EmailService {
 		sm.setText(obj.toString());
 		return sm;
 	}
+
+	/** Montar o HTML do email
+	 */
+	protected String htmlFromTemplatePedido(Pedido obj) {
+		
+		Context context = new Context();	// context do thymeleaf
+		context.setVariable("pedido", obj);	//manda para a pagina thymeleaf
+		return templateEngine.process("email/confirmacaoPedido", context);		
+	}
 	
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj) {	
+		
+		// se nao mandar o HTML, manda o texto plano no email
+		try {
+			MimeMessage mm = prepareMimeMessageFromPedido(obj);
+			sendHtmlEmail(mm);
+		} catch (MessagingException e) {
+			sendOrderConfirmationEmail(obj);
+		}
+	}
+
+	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+		MimeMessage mm = javaMailSender.createMimeMessage();
+		MimeMessageHelper mmh = new MimeMessageHelper(mm, true);
+		mmh.setTo(obj.getCliente().getEmail());
+		mmh.setFrom(sender);
+		mmh.setSubject("Pedido Confirmado! Código: " + obj.getId());
+		mmh.setSentDate(new Date(System.currentTimeMillis()));
+		mmh.setText(htmlFromTemplatePedido(obj), true);	// true diz ue é HTML		
+		
+		return mm;
+	}
 }
--- src/main/java/com/ivson/modelagemconceitual/services/EmailService.java
+++ src/main/java/com/ivson/modelagemconceitual/services/EmailService.java
@@ -1,13 +1,21 @@
 package com.ivson.modelagemconceitual.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.ivson.modelagemconceitual.model.Pedido;
 
 public interface EmailService {
 
+	// enviar email normal
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	// enviar email com HTML
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	// para enviar email HTML MimeMessage
+	void sendHtmlEmail(MimeMessage msg);	 
 	
 }
